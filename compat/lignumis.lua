c92d8d814ef7e2b2f36b92d87cd30d5a077f3e51
Syen-ce <angellecce2003@gmail.com>
2026-03-31 12:22:17 +0200

    1.1.25

--- compat/lignumis.lua
+++ compat/lignumis.lua
@@ -13,3 +13,14 @@ cactus_wood.ingredients =
         { type = "item", name = "planetaris-cactus", amount = 1 },
       }
 cactus_wood.results = { { type = "item", name = "wood", amount = 4 } }
+
+local arig_rock = data.raw["simple-entity"]["arig-big-sand-rock"].minable.results
+table.insert(arig_rock,
+    {type = "item", name = "gold-ore", amount_min = 19, amount_max = 25})
+
+table.insert(arig_crash,
+    {type = "item", name = "gold-plate", amount_min = 5, amount_max = 15})
+
+local planetaris_sand_sifting = data.raw["recipe"]["planetaris-sand-sifting"].results
+table.insert(planetaris_sand_sifting,
+    {type = "item", name = "gold-ore", amount = 1, probability = 0.05, show_details_in_recipe_tooltip = false})
